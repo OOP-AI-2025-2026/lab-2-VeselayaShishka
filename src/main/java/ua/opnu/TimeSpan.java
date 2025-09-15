beb85d4a65e691d78d74b45f0b6d82369b992f71
VeselayaShishka <protomax040@gmail.com>
2025-09-15 19:13:23 +0300

    Update TimeSpan.java

--- src/main/java/ua/opnu/TimeSpan.java
+++ src/main/java/ua/opnu/TimeSpan.java
@@ -5,42 +5,105 @@ public class TimeSpan {
 
     // TODO: add class fields
 
+    int hours, minutes;
+
+
     TimeSpan(int hours, int minutes) {
-        // TODO: write constructor body
+
+        if (hours < 0 || minutes < 0|| minutes > 59) {
+            return;
+        }
+
+        this.hours = hours;
+        this.minutes = minutes;
     }
 
     int getHours() {
-        return 0;
+        return hours;
     }
 
     int getMinutes() {
         // TODO: write method body
-        return 0;
+        return minutes;
     }
 
     void add(int hours, int minutes) {
+
+        if (hours < 0 || minutes < 0|| minutes > 59) {
+            return;
+        }
+
+        this.minutes += minutes;
+
+        if (this.minutes > 59){
+
+            this.hours += (hours + this.minutes / 60);
+            this.minutes = this.minutes - 60;
+
+            return;
+        }
+
+        this.hours += hours;
         // TODO: write method body
     }
 
     void addTimeSpan(TimeSpan timespan) {
+
+        this.minutes += timespan.getMinutes();
+
+        if (this.minutes > 59){
+
+            this.hours += (timespan.getHours() + this.minutes / 60);
+            this.minutes = this.minutes - 60;
+
+            return;
+        }
+
+        this.hours += timespan.getHours();
+
         // TODO: write method body
     }
 
     double getTotalHours() {
+
+        double min = this.minutes;
+
         // TODO: write method body
-        return 0;
+        return this.hours + (((min * 100)/60))/100;
     }
 
     int getTotalMinutes() {
-        // TODO: write method body
-        return 0;
+
+        return this.minutes + (this.hours * 60);
     }
 
     void subtract(TimeSpan span) {
+
+        int a =  this.hours*60 + this.minutes;
+        int b = span.hours*60 + span.minutes;
+
+        if(a < b){
+            throw new IllegalArgumentException();
+        }
+
+        this.hours = (a-b)/60;
+        this.minutes = (a-b)%60;
+
         // TODO: write method body
     }
 
     void scale(int factor) {
+
+        if(factor < 0){
+            throw new IllegalArgumentException();
+        }
+        int a =  this.hours*60 + this.minutes;
+
+        int b = a*factor;
+
+        this.hours = (b)/60;
+        this.minutes = (b)%60;
+
         // TODO: write method body
     }
 }
